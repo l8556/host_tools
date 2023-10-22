6b06dfc4df65be4ea947d2447771998e27a99d5e
Nikolai Lambrov <52069739+l8556@users.noreply.github.com>
2023-10-22 10:00:16 +0300

    added get name method (#20)

--- host_control/utils/Process.py
+++ host_control/utils/Process.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import psutil
 from psutil import process_iter
 from rich import print
 
@@ -6,8 +7,15 @@ from rich import print
 def terminate(names: list) -> None:
     for process in process_iter():
         for terminateProcess in names:
-            if terminateProcess in process.name():
+            if terminateProcess in get_name(process):
                 try:
                     process.terminate()
                 except Exception as e:
                     print(f'[bold red]|Warning| Exception when terminate process {terminateProcess}: {e}')
+
+
+def get_name(process):
+    try:
+        return process.name()
+    except psutil.NoSuchProcess as e:
+        print(f"[bold red]|ERROR| Process name not found: {e}")
