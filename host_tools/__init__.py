6f46f9264eef1d61f5cef8ce1062c047c525ef4c
l02 <l8556@outlook.com>
2023-12-12 20:08:43 +0300

    .

--- host_tools/__init__.py
+++ host_tools/__init__.py
@@ -2,6 +2,4 @@
 from .utils import Dir, Str, Shell, File, Process
 from .info import HostInfo
 from .singleton import singleton
-
-if HostInfo().os == 'windows':
-    from .windows_handler import Window
+from .windows_handler import Window
