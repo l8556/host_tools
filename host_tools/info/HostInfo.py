c0ab2232adbe4b633a96c63abe2cfec7150ab027
l02 <l8556@outlook.com>
2024-07-09 12:52:24 +0300

    fix_macOs_detection

--- host_tools/info/HostInfo.py
+++ host_tools/info/HostInfo.py
@@ -16,9 +16,14 @@ class HostInfo:
     def os(self) -> str:
         if self.__os is None:
             self.__os = system().lower()
+
             if self.__os not in ['linux', 'darwin', 'windows']:
                 print(f"[bold red]|ERROR| Error defining os: {self.__os}")
                 self.__os = ''
+
+            elif self.__os == 'darwin':
+                self.__os == 'mac'
+
         return self.__os
 
     @property
@@ -40,3 +45,5 @@ class HostInfo:
             else:
                 self.__version = Unix().version
         return self.__version
+
+
