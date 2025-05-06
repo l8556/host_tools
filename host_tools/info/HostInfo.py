547b4c53423bfdf050a6dddff556596533302f50
Nikolai Lambrov <52069739+l8556@users.noreply.github.com>
2025-05-06 15:50:27 +0300

    Feature/added new methods to hostinfo (#86)
    * feature added new methods to Hostinfo
    
    * feature added new methods to Hostinfo
    
    ---------
    
    Co-authored-by: Nikolai Lambrov <nikolay.lambrov@onlyoffice.com>

--- host_tools/info/HostInfo.py
+++ host_tools/info/HostInfo.py
@@ -7,6 +7,12 @@ from ..singleton import singleton
 
 @singleton
 class HostInfo:
+    __windows = 'windows'
+    __linux = 'linux'
+    __mac = 'mac'
+    __darwin = 'darwin'
+    __supported_os = [__windows, __linux, __mac, __darwin]
+
     def __init__(self):
         self.__os = None
         self.__arch = None
@@ -18,14 +24,26 @@ class HostInfo:
         if self.__os is None:
             self.__os = system().lower()
 
-            if self.__os not in ['linux', 'darwin', 'windows']:
+            if self.__os not in self.__supported_os:
                 print(f"[bold red]|ERROR| Error defining os: {self.__os}")
                 self.__os = ''
 
-            if self.__os == 'darwin':
-                self.__os = 'mac'
+            if self.__os == self.__darwin:
+                self.__os = self.__mac
 
         return self.__os
+
+    @property
+    def is_windows(self) -> bool:
+        return self.os == self.__windows
+
+    @property
+    def is_linux(self) -> bool:
+        return self.os == self.__linux
+
+    @property
+    def is_mac(self) -> bool:
+        return self.os == self.__mac
 
     @property
     def arch(self) -> str:
@@ -34,7 +52,7 @@ class HostInfo:
         return self.__arch
 
     def name(self, pretty: bool = False) -> str:
-        if self.os == 'windows':
+        if self.is_windows:
             return f"{self.os} {self.release}" if pretty else self.os
         return Unix().pretty_name if pretty else Unix().id
 
@@ -47,7 +65,7 @@ class HostInfo:
     @property
     def version(self) -> str:
         if self.__version is None:
-            if self.os == 'windows':
+            if self.is_windows:
                 self.__version = version()
             else:
                 self.__version = Unix().version
